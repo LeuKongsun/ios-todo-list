75dcc84436dcab2fb70b8abbafbe598a6a3df67f
Kongsun Leu <kongsun79@gmail.com>
2023-07-05 10:52:14 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Kongsun on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
